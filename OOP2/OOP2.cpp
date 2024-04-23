529157bf18fe809d4a235d2267e9350f2d2afdfe
adilaroisa <adilaroisa688@gmail.com>
2024-04-23 11:11:40 +0700

    membuat fungsi display

--- OOP2/OOP2.cpp
+++ OOP2/OOP2.cpp
@@ -20,6 +20,12 @@ class bangunDatar {
         float hitungLuas() {
             return panjang * lebar;
         }
+
+        void display() {
+            cout << "Panjangny = " << panjang << endl;
+            cout << "Lebarnya = " << lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+        }
 }; 
 
 
