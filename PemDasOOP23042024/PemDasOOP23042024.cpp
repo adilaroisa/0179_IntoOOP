064c0207b499245111d5629acdd1c7685019473b
adilaroisa <adilaroisa688@gmail.com>
2024-04-23 09:58:15 +0700

    menampilkan informasi mahasiswa

--- PemDasOOP23042024/PemDasOOP23042024.cpp
+++ PemDasOOP23042024/PemDasOOP23042024.cpp
@@ -6,6 +6,12 @@ class Mahasiswa {
         string nama;
         int umur;
         string jurusan;
+
+        void output() {
+            cout << "Nama: " << nama << endl;
+            cout << "Umur: " << umur << endl;
+            cout << "Jurusan: " << jurusan << endl;
+        }
   
 };
 
